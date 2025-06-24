922cc97075d174ec44b887a6ba77211985e63f10
Jay S <thedorkgames@gmail.com>
2025-06-24 18:05:12 -0500

    classes

--- global.js
+++ global.js
@@ -7,3 +7,30 @@ function file(name) {
 function rgba(r,g,b,a=255) {
     return `rgba(${r},${g},${b},${a})`
 }
+//JSON Right Bracket Removed
+function JSONRBR (obj) {
+    var a = JSON.stringify(obj)
+    a.pop()
+    return a
+}
+
+class wheel {
+    constructor(name) {
+    this.name = name;
+    this.spots = []
+    }
+}
+
+class wheelSpot {
+    constructor(name,colour, weight) {
+    this.name = name;
+    this.colour = colour
+    this.modes = {
+        base:weight
+    }
+    }
+    addMode(key, value) {
+        this.modes = JSON.parse(`${JSONRBR(this.modes)},"${key}":${value}}`)
+    }
+}
+
