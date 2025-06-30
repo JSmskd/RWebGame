58ae4ad1b7f4b74888efecb34ebd3f7862925820
Jay S <thedorkgames@gmail.com>
2025-06-29 22:00:34 -0500

    removed unknown part of liny boo

--- compile/run.py
+++ compile/run.py
@@ -1,4 +1,3 @@
-import subprocess
 import platform
 import os
 
@@ -6,22 +5,16 @@ ext = r""
 
 if platform.system().startswith("Win"):
     ext = r"bat"
-elif platform.system().startswith("Lin"):
-    ext = r"sh"
+# elif platform.system().startswith("Lin"):
+    # ext = r"sh"
 else:
     exit()
 
-print("n\n\n\n\n")
-d = os.path.abspath(r"compile\\" + r"compile." + ext)
-print(d)
+os.system(os.path.abspath(r"compile\\" + r"compile." + ext))
 
-os.system(d)
-# subprocess.run([d])#.communicate()
-
-if None == None:
-    with open(os.path.abspath(r"compile\\global.js")) as f:
-        with open(os.path.abspath(r"webpage\\global.js"), "w") as ff:
-            ff.write(f.read())
-            ff.close()
-        f.close()
-        os.remove(os.path.abspath(r"compile\\global.js"))
+with open(os.path.abspath(r"compile\\global.js")) as f:
+    with open(os.path.abspath(r"webpage\\global.js"), "w") as ff:
+        ff.write(f.read())
+        ff.close()
+    f.close()
+    os.remove(os.path.abspath(r"compile\\global.js"))
